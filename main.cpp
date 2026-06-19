926cacf0b9426192899c906a09cd7cdbaa8196f0
Lyq2333333 <kaoshenbiyou@qq.com>
2026-06-19 17:49:37 +0800

    背景颜色改为蓝色

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
     initgraph(640, 480);        // 创建 640x480 窗口
-    setbkcolor(WHITE); 
-    cleardevice();          // 设置背景颜色为白色
+    setbkcolor(BLUE); 
+    cleardevice();          // 设置背景颜色为蓝色
     setcolor(RED);              // 设置颜色为红色
     circle(320, 240, 100);      // 在中心画半径为100的圆
     getchar();                  // 按任意键继续
