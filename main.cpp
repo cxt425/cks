1234de945e625bc7186f38a64221a093ad382367
Lyq2333333 <kaoshenbiyou@qq.com>
2026-07-05 14:18:59 +0800

    新增绘制个人电动车管理主界面

--- main.cpp
+++ main.cpp
@@ -1,13 +1,68 @@
 #include <graphics.h>
 #include <stdio.h>
+int main() 
+{
+    initgraph(480, 640);        // 创建 480x640 窗口
+    setbkcolor(WHITE);
+    cleardevice();              // 设置背景颜色为白色
+    setfillcolor(RGB(0,146,198)); // 设置填充颜色为蓝色
+    setlinecolor(RGB(0,146,198)); // 新增统一边框颜色
+    fillrectangle(30,335,210,425); //左上卡片【车辆注册/上牌】
+    fillrectangle(270,335,450,425); //右上卡片【车辆年审管理】
+    fillrectangle(30,440,210,530);//左下框【车辆信息变更】
+    fillrectangle(270,440,450,530);//右下框【车辆报废管理】
+    fillrectangle(30,545,450,605);//底部通栏长框【出入校园管理】
 
-int main() {
-    initgraph(640, 480);        // 创建 640x480 窗口
-    setbkcolor(BLUE); 
-    cleardevice();          // 设置背景颜色为蓝色
-    setcolor(RED);              // 设置颜色为红色
-    circle(320, 240, 100);      // 在中心画半径为100的圆
-    getchar();                  // 按任意键继续
-    closegraph();               // 关闭图形窗口
+    IMAGE img;
+    loadimage(&img, _T("xiaohui.png"), 120, 110);// 加载图片文件 "xiaohui.png"，并将其缩放为 120x110 像素
+    putimage(0, 70, &img);// 在坐标 (0, 70) 位置显示图片
+
+    settextstyle(50, 0, _T("华文行楷"));//设置文字样式：字号 50、方向 0、字体为“华文行楷”
+    settextcolor(RGB(0,75,132));// 设置文字颜色为蓝色
+    outtextxy(120, 100, _T("华中科技大学"));//在坐标 (120, 100) 位置显示文字
+     
+    settextstyle(15, 0, _T("Spectral"));//设置文字样式：字号 15、方向 0、字体为“Spectral”
+    settextcolor(RGB(0,75,132));//设置文字颜色为蓝色
+    outtextxy(100, 145, _T("HUAZHONG UNIVERSITY OF SCIENCE AND TECHNOLOGY"));//在坐标 (100, 145) 位置显示文字
+
+    settextstyle(20,0,_T("黑体"));
+    outtextxy(135,200,_T("个人电动车登记管理系统"));//界面说明：个人电动车管理系统
+    settextcolor(WHITE);//卡片内白色文字
+    
+    setbkmode(TRANSPARENT);//设置文字背景为透明
+    
+    settextstyle(20,0,_T("黑体"));
+    outtextxy(60,355,_T("车辆注册/上牌"));//左上卡片文字
+    settextstyle(14,0,_T("黑体"));
+    outtextxy(35,382,_T("新增车辆录入/车牌查重校验"));//左上卡片小文字
+   
+    settextstyle(20,0,_T("黑体"));
+    outtextxy(300,355,_T("车辆年审管理"));//右上卡片文字
+    settextstyle(14,0,_T("黑体"));
+    outtextxy(275,382,_T("修改年审日期/更新年审状态"));//右上卡片小文字
+    
+    settextstyle(20,0,_T("黑体"));
+    outtextxy(60,460,_T("车辆信息变更"));//左下卡片文字
+    settextstyle(14,0,_T("黑体"));
+    outtextxy(35,486,_T("车主信息修改/车辆备注修改"));//左下卡片小文字
+    
+    settextstyle(20,0,_T("黑体"));
+    outtextxy(300,460,_T("车辆报废管理"));//右下卡片文字
+    settextstyle(14,0,_T("黑体"));
+    outtextxy(275,486,_T("报废车辆处理/更新报废状态"));//右下卡片小文字
+    
+    settextstyle(22,0,_T("黑体"));
+    outtextxy(175,560,_T("出入校园管理"));//底部通栏长框文字
+    
+    settextstyle(14,0,_T("黑体"));
+    outtextxy(160,590,_T("记录出入时间/更新校园状态"));//底部通栏长框小文字
+
+    settextcolor(BLACK);
+    settextstyle(12,0,_T("黑体"));
+    outtextxy(120,615,_T("当前登录:学号123456789|系统版本:V1.0"));//底部版权信息
+
+
+    getchar();                  //按任意键继续
+    closegraph();               //关闭图形窗口
     return 0;
 }
