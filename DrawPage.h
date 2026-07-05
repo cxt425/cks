0e69b2bb14ed7717b3972d5d7c7951b52e8ecf9b
Lyq2333333 <kaoshenbiyou@qq.com>
2026-07-05 14:29:11 +0800

    添加绘制个人电动车管理界面

--- DrawPage.h
+++ DrawPage.h
@@ -2,4 +2,5 @@
 #define DRAWPAGE_H    // 定义 DRAWPAGE_H，防止头文件被重复包含
 void DrawFirstPage(void);    // 声明 DrawFirstPage 函数，用于绘制首页界面
 void DrawSharedSignoutPage(void);  // 声明 DrawSharedSignoutPage 函数，用于绘制共享电动车登录首页
+void DrawPersonalManagementPage(void);    // 声明 DrawPersonalManagementPage 函数，用于绘制个人电动车管理系统界面
 #endif                       // 结束包含保护
