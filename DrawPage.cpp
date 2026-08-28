7682b670276263b0bd0481925dec90090706584c
Lyq2333333 <kaoshenbiyou@qq.com>
2026-08-28 15:31:27 +0800

    共享登录鼠标控制

--- DrawPage.cpp
+++ DrawPage.cpp
@@ -781,3 +781,8 @@ void DrawSharedManagementPage()//定义 DrawSharedManagementPage 函数，用于
     int tip_w=textwidth(_T("请选择您的操作"));
     outtextxy(240-tip_w/2, 580, _T("请选择您的操作"));//底部提示文字 
 }
+
+void DrawSharedUseVehiclePage(void)
+{
+    
+}
